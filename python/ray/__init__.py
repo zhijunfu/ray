72aa7cd765fd2048e2a7d47a7fcd3580869d2c24
songqing <zhangsongqing164@163.com>
2018-06-11 08:13:22 +0800

    lint fix (#6)

--- python/ray/__init__.py
+++ python/ray/__init__.py
@@ -47,11 +47,10 @@ except ImportError as e:
     raise
 
 from ray.local_scheduler import ObjectID, _config  # noqa: E402
-from ray.worker import (error_info, init, connect, disconnect, get, put, 
-                        create_queue, push_queue, read_queue, wait,
-                        remote, log_event, log_span, flush_log, get_gpu_ids,
-                        get_webui_url,
-                        register_custom_serializer)  # noqa: E402
+from ray.worker import (
+    error_info, init, connect, disconnect, get, put, create_queue, push_queue,
+    read_queue, wait, remote, log_event, log_span, flush_log, get_gpu_ids,
+    get_webui_url, register_custom_serializer)  # noqa: E402
 from ray.worker import (SCRIPT_MODE, WORKER_MODE, PYTHON_MODE,
                         SILENT_MODE)  # noqa: E402
 from ray.worker import global_state  # noqa: E402
@@ -65,12 +64,11 @@ from ray.actor import method  # noqa: E402
 __version__ = "0.4.0"
 
 __all__ = [
-    "error_info", "init", "connect", "disconnect", "get", "put", "wait",
-    "create_queue", "push_queue", "read_queue", 
-    "remote", "log_event", "log_span", "flush_log", "actor", "method",
-    "get_gpu_ids", "get_webui_url", "register_custom_serializer",
-    "SCRIPT_MODE", "WORKER_MODE", "PYTHON_MODE", "SILENT_MODE", "global_state",
-    "ObjectID", "_config", "__version__"
+    "error_info", "init", "connect", "disconnect", "get", "put",
+    "create_queue", "push_queue", "read_queue", "wait", "remote", "log_event",
+    "log_span", "flush_log", "actor", "method", "get_gpu_ids", "get_webui_url",
+    "register_custom_serializer", "SCRIPT_MODE", "WORKER_MODE", "PYTHON_MODE",
+    "SILENT_MODE", "global_state", "ObjectID", "_config", "__version__"
 ]
 
 import ctypes  # noqa: E402
