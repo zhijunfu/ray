0f0d43aa42410295f129df59dca166a01f1c019c
Zhijun Fu <pingfu.fzj@antfin.com>
2018-07-18 10:36:14 +0800

    [patch] apply patch for supporting java raylet

--- java/runtime-common/src/main/java/org/ray/spi/ObjectStoreProxy.java
+++ java/runtime-common/src/main/java/org/ray/spi/ObjectStoreProxy.java
@@ -11,6 +11,7 @@ import org.ray.api.UniqueID;
 import org.ray.api.WaitResult;
 import org.ray.core.Serializer;
 import org.ray.core.WorkerContext;
+import org.ray.spi.LocalSchedulerLink;
 import org.ray.util.exception.TaskExecutionException;
 import org.ray.util.logger.RayLog;
 
@@ -21,11 +22,18 @@ import org.ray.util.logger.RayLog;
 public class ObjectStoreProxy {
 
   private final ObjectStoreLink store;
+  private final LocalSchedulerLink localSchedulerLink;
   private final int getTimeoutMs = 1000;
 
   public ObjectStoreProxy(ObjectStoreLink store) {
     this.store = store;
+    this.localSchedulerLink = null;
   }
+
+  public ObjectStoreProxy(ObjectStoreLink store, LocalSchedulerLink localSchedulerLink) {
+    this.store = store;
+    this.localSchedulerLink = localSchedulerLink;
+  } 
 
   public <T> Pair<T, GetStatus> get(UniqueID objectId, boolean isMetadata)
       throws TaskExecutionException {
@@ -130,7 +138,12 @@ public class ObjectStoreProxy {
     for (RayObject<T> obj : waitfor.Objects()) {
       ids.add(obj.getId());
     }
-    List<byte[]> readys = store.wait(getIdBytes(ids), timeout, numReturns);
+    List<byte[]> readys;
+    if (localSchedulerLink == null) {
+      readys = store.wait(getIdBytes(ids), timeout, numReturns);
+    } else {
+      readys = localSchedulerLink.wait(getIdBytes(ids), timeout, numReturns);
+    }
 
     RayList<T> readyObjs = new RayList<>();
     RayList<T> remainObjs = new RayList<>();
@@ -146,11 +159,19 @@ public class ObjectStoreProxy {
   }
 
   public void fetch(UniqueID objectId) {
-    store.fetch(objectId.getBytes());
+    if (localSchedulerLink == null) {
+      store.fetch(objectId.getBytes());
+    } else {
+      localSchedulerLink.fetch(objectId);
+    }
   }
 
   public void fetch(List<UniqueID> objectIds) {
-    store.fetch(getIdBytes(objectIds));
+    if (localSchedulerLink == null) {
+      store.fetch(getIdBytes(objectIds));
+    } else {
+      localSchedulerLink.fetch(objectIds);
+    }
   }
 
   public int getFetchSize() {
